0b902393ea1533dad00634a198252ff4653899e3
Sewonzzang123 <dkrlrhdrur@gmail.com>
2021-05-20 11:50:16 +0900

    Preparedstatement 연습

---
+++ mariadb-practice/jdbc3/src/main/java/test/InsertTest02.java
@@ -0,0 +1,67 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class InsertTest02 {
+
+	public static void main(String[] args) {
+		insertDepartment("영업");
+		insertDepartment("개발");
+		insertDepartment("기획");
+	}
+		
+	
+	
+	public static boolean insertDepartment(String name) {	
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		boolean result = false;
+		try {
+			// maven-dependencies에 없을 경우 classNotFound
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			// tcp,ip연결
+			String url = "jdbc:mysql://192.168.80.112:3307/employees";
+			String id = "hr";
+			String password = "hr";
+			conn = DriverManager.getConnection(url, id, password);
+
+			// sql문 준비 (sql문 안에 ?로 비워둠)
+			String sql = "insert "
+					+ " into dept "
+					+ " values(null,?)";
+			pstmt = conn.prepareStatement(sql);
+			
+			// binding ( ? 값이 name으로 채워짐)
+			pstmt.setString(1, name);
+			
+			// sql 실행		
+			int count = pstmt.executeUpdate();
+			
+			result = (count == 1);
+		
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: " + e);
+		} catch (SQLException e) {
+			System.out.println("error " + e);
+		} finally {
+			try {
+
+				if (pstmt != null) {
+					pstmt.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error " + e);
+			}
+		}
+		return result;
+	}
+
+}
---
+++ mariadb-practice/jdbc3/src/main/java/test/UpdateTest02.java
@@ -0,0 +1,64 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class UpdateTest02 {
+
+	public static void main(String[] args) {
+		DeptVo vo = new DeptVo();
+		vo.setNo(4L);
+		vo.setName("전략기획");
+		Boolean result = update(vo);	
+		System.out.println(result ? "성공":"실패");
+	}
+	public static boolean update(DeptVo vo) {
+		Connection conn = null;
+		Statement stmt = null;
+		boolean result = false;
+		try {
+			// maven-dependencies에 없을 경우 classNotFound
+			Class.forName("org.mariadb.jdbc.Driver");
+
+			// tcp,ip연결
+			String url = "jdbc:mysql://192.168.80.112:3307/employees";
+			String id = "hr";
+			String password = "hr";
+			conn = DriverManager.getConnection(url, id, password);
+
+			// Statement 생성
+			stmt = conn.createStatement();
+			
+			//sql문을 실행
+			String sql = "update dept "
+					+ " set name='"+vo.getName()+"' "
+					+ " where no="+vo.getNo();
+			
+			int count = stmt.executeUpdate(sql);
+			
+			result = (count == 1);
+		
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패: " + e);
+		} catch (SQLException e) {
+			System.out.println("error " + e);
+		} finally {
+			try {
+
+				if (stmt != null) {
+					stmt.close();
+				}
+
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				System.out.println("error " + e);
+			}
+		}
+		return result;
+	}
+
+}
